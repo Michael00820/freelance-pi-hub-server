a5f01665f9caa8359375b42153c91a8b1cb473d5
Michael00820 <37353304+Michael00820@users.noreply.github.com>
2025-08-21 01:05:49 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,41 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
+// models/User.js
+import { DataTypes } from "sequelize";
 
-const User = sequelize.define('User', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
-}, {
-  timestamps: true
-});
+export default function createUser(sequelize) {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(120),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(160),
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      passwordHash: {
+        type: DataTypes.STRING(120),
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("freelancer", "client"),
+        allowNull: false,
+        defaultValue: "freelancer",
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+      indexes: [{ unique: true, fields: ["email"] }],
+    }
+  );
 
-module.exports = User;
+  return User;
+}
